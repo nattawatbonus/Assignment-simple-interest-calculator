2ecfd82680fae66942df60b25541aa6d9d739c1a
Nattawat Konthongdee <102992075+nattawatbonus@users.noreply.github.com>
2022-05-23 02:38:42 +0700

    Add files via upload

--- script.js
+++ script.js
@@ -1,6 +1,25 @@
-function compute()
-{
-    p = document.getElementById("principal").value;
-    
+function compute() {
+    var principal = document.getElementById("principal").value;
+    if (parseInt(principal) < 1) {
+        alert("Enter a positive number");
+        document.getElementById("principal").focus();
+        return;
+    }
+    var rate = document.getElementById("rate").value
+    var years = document.getElementById("years").value;
+    var year = new Date().getFullYear() + parseInt(years);
+    var amount = principal * years * rate / 100;
+    document.getElementById("resultdeposit").innerHTML = "" + principal;
+    document.getElementById("resultinterest").innerHTML = "" + rate + "%";
+    document.getElementById("resultamount").innerHTML = "" + amount;
+    document.getElementById("resultyear").innerHTML = "" + year;
+
+    return false;
 }
+
+function updateRate() {
+    var rateval = document.getElementById("rate").value;
+    document.getElementById("rate_val").innerText = rateval;
+}
+
         
